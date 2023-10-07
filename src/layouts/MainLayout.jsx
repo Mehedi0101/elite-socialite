5906f14c4c318d55706cdb8ae329264848b65b18
Mehedi0101 <mehedi29121998@gmail.com>
2023-10-07 23:33:53 +0600

    login and registration functionalities added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/shared components/Footer";
 import Navbar from "../components/shared components/Navbar";
+import ScrollToTop from "../utils/scrollToTop";
 
 const MainLayout = () => {
     return (
         <div>
+            <ScrollToTop />
             <Navbar></Navbar>
             <div className="min-h-[60vh]">
                 <Outlet></Outlet>
